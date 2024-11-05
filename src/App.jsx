bb856ac9b61a59016faadffee19fe499c8597c2a
Nicarl0s <joaquinreynoso627@gmail.com>
2024-11-05 01:01:34 +0000

    si

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Profile from './pages/Profile'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
 import ProtectedRoute from "./ProtectedRoute.jsx";
 import BookDataForm from "./pages/BookDataForm.jsx";
 import Home from "./pages/Home.jsx";
 import Product from "./pages/Product.jsx";
 import Portfolio from "./pages/Porfolio.jsx";
+import Contact from "./pages/Contact.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
 
 function App() {
     return (
@@ -19,12 +21,20 @@ function App() {
                     <Route path="/books" element={<BookDataForm />}/>
                     <Route path="/home" element={<Home />}/>
                     <Route path="/product" element={<Product />}/>
-                    <Route path="/portfolio" element={<Portfolio />}/>
+                    <Route path="/portfolio/reading" element={<Portfolio />}/>
+                    <Route path="/portfolio/favorites" element={<Portfolio />}/>
+                    <Route path="/portfolio/comments" element={<Portfolio />}/>
+                    <Route path="/aboutUs/developers" element={<AboutUs />}/>
+                    <Route path="/aboutUs/project" element={<AboutUs />}/>
+                    <Route path="/contact" element={<Contact />}/>
                     <Route path="/login" element={<Login />}/>
                     <Route path="/register" element={<Register />}/>
                     <Route element={<ProtectedRoute />}>
                         <Route path="/user-profile" element={<Profile />}/>
                     </Route>
+                    <Route path="/portfolio/*" element={<Navigate to="/portfolio/reading" replace />} />
+                    <Route path="/aboutUs/*" element={<Navigate to="/aboutUs/developers" replace />} />
+                    <Route path="/portfolio" element={<Navigate to="/portfolio/reading" replace />}/>
                     <Route path="*" element={<Navigate to="/home" replace />} />
                 </Routes>
             </main>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,9 @@
 import { NavLink } from "react-router-dom";
+import { Cards } from "../components/Cards";
 
 const Home = () => {
-    const prods = [
-        ['Book Title 1', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus architecto nostrum voluptatum illum qui obcaecati labore. Consectetur, voluptatibus soluta ducimus repudiandae quam obcaecati facilis. Vitae labore sunt consectetur fuga dolor?', '1248177874835'],
-        ['Book Title 2', 'Book Resume 2', '1389247837582'],
-        ['Book Title 3', 'Book Resume 3', '2365452612634'],
-        ['Book Title 4', 'Book Resume 4', '1352352353535'],
-        ['Book Title 5', 'Book Resume 5', '1231245236534'],
-        ['Book Title 6', 'Book Resume 6', '1235899234515'],
-        ['Book Title 7', 'Book Resume 7', '9586943787213'],
-        ['Book Title 8', 'Book Resume 8', '3859115636543'],
-        ['Book Title 9', 'Book Resume 9', '3587918245363']
-    ]
-
     return (
-       <div className="cardsContainer overflow-y-scroll no-scrollbar h-full w-full transition ease-in-out py-[2vw] px-[5vw] grid sm:grid-cols-3 lg:grid-cols-4 gap-9">
-            {prods.map((element) => (
-                <NavLink to={'/product?prod=' + element[2]} key={element[2]} className="card hover:cursor-pointer hover:scale-110 duration-300 bg-black grid grid-rows-2 justify-items-center rounded-md p-2 overflow-hidden h-[30rem] sm:h-[18rem] xl:h-[50vh]">
-                    <img src="" alt="Book" className="tape w-full bg-white h-full rounded-md" />
-                    <div className="text-stone-50 h-[100%] w-10/12 mt-2 flex flex-col justify-between">
-                        <h1 className="text-center text-xl font-bold">{element[0]}</h1>
-                        <p className="overflow-hidden my-1">
-                            {element[1]}
-                        </p>
-                        <p className='text-sm mb-1 block text-center font-semibold button mt-2' >
-                            Ver Mas...
-                        </p>
-                    </div>
-                </NavLink>
-            ))}
-       </div>
+        <Cards />
     );
 };
 
